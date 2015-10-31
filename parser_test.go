619d2f173e753dc364048c4c100d73f0eb02b2d3
Ben Johnson <benbjohnson@yahoo.com>
2015-10-30 20:21:02 -0600

    add recursive parsing and working directories
    Signed-off-by: Ben Johnson <benbjohnson@yahoo.com>

--- parser_test.go
+++ parser_test.go
@@ -10,25 +10,25 @@ import (
 
 // Ensure a dependency-less target can be parsed.
 func TestParser_Parse_NoDependencies(t *testing.T) {
-	p, err := bake.NewParser().ParseString(`
+	p := bake.NewParser()
+	if err := p.ParseString("Bakefile", `
 target("bin/flynn-host", function()
 	exec "run1.sh"
 	exec "run2.sh"
 end)
-`)
-	if err != nil {
+`); err != nil {
 		t.Fatal(err)
 	}
 
 	// Retrieve target from package.
-	target := p.Target("bin/flynn-host")
+	target := p.Package.Target("bin/flynn-host")
 	if target == nil {
 		t.Fatal("expected target")
 	} else if len(target.Inputs) != 0 {
 		t.Fatalf("unexpected inputs: %v", target.Inputs)
 	} else if !reflect.DeepEqual(target.Commands, []bake.Command{
-		&bake.ExecCommand{Text: "run1.sh"},
-		&bake.ExecCommand{Text: "run2.sh"},
+		&bake.ExecCommand{Args: []string{"run1.sh"}},
+		&bake.ExecCommand{Args: []string{"run2.sh"}},
 	}) {
 		t.Fatalf("unexpected commands: %s", spew.Sdump(target.Commands))
 	}
@@ -36,15 +36,15 @@ end)
 
 // Ensure a target can be parsed with dependencies.
 func TestParser_Parse_Dependencies(t *testing.T) {
-	p, err := bake.NewParser().ParseString(`
+	p := bake.NewParser()
+	if err := p.ParseString("Bakefile", `
 target("bin/flynn-host", depends("A", "B"), function() end)
-`)
-	if err != nil {
+`); err != nil {
 		t.Fatal(err)
 	}
 
 	// Retrieve target from package.
-	target := p.Target("bin/flynn-host")
+	target := p.Package.Target("bin/flynn-host")
 	if target == nil {
 		t.Fatal("expected target")
 	} else if !reflect.DeepEqual(target.Inputs, []string{"A", "B"}) {
